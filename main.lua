b713521b926e1498afc970f379cf479f92026527
Dayton Daniels <67416737+MrTorchic@users.noreply.github.com>
2022-03-03 16:27:34 -0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -13,20 +13,6 @@ b:onClick(function(button, frame, event, x, y)
     frame:getScreen():stop()
 end)
 
-local unlock = s:add(gui.button("Activate"))
-unlock:setTextColor(colors.white)
-unlock:setBackgroundColor(colors.green)
-unlock:setBackgroundColor(1,1)
-unlock:onClick(function(button, frame, event, x, y)
-    local deactivate = s:add(gui.button("Deactivate"))
-     deactivate:setTextColor(colors.white)
-     deactivate:setBackgroundColor(colors.red)
-     deactivate:setPosition(1,1)
-     deactivate:onClick(function(button,frame,event,x,y)
-    
-         )
-    end)
-end)
 
 
 
